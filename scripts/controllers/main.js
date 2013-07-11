eaa69d8731aed50348e2c7cd09c4f40154928437
unknown <cristobal.avila@MW79OIX4M5XYHN.dir.svc.accenture.com>
2013-07-11 14:51:30 -0600

    crypto lib added

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -12,12 +12,38 @@ angular.module('cryptoballApp')
     $scope.inputMessage = "";
     $scope.extraMsg = "LOL";
     $scope.newMsg = "";
+
+    $scope.encrypted = "";
+    $scope.desencrypted = "";
+
+//https://code.google.com/p/crypto-js/
+
+/*
+Example
+
+var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+decrypted.toString(CryptoJS.enc.Utf8);
+
+ */
+
     /**
      * [ concatenates two strings and assigns into a third parameter ]
      * @return {string} [description]
      */
     $scope.concatString = function() {
-    	$scope.newMsg = $scope.inputMessage + $scope.extraMsg;
+    	$scope.newMsg = CryptoJS.AES.encrypt($scope.inputMessage, $scope.extraMsg);
+    };
+
+    $scope.encryptor = function() {
+      $scope.encrypted = CryptoJS.AES.encrypt($scope.inputMessage, $scope.extraMsg);
+      //var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+
+    };
+
+    $scope.desencryptor = function() {
+      $scope.newMsg = CryptoJS.AES.encrypt($scope.inputMessage, $scope.extraMsg);
+      //var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
     };
     
   });
