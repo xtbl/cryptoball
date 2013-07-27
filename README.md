c3ff062e469301750659104171f52dab300f087a
xtbl <correodecristobal@gmail.com>
2013-07-26 23:18:26 -0600

    styles added, v1.0

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 cryptoball
 ==========
 
-Encrypt and decrypt webapp. AngularJS.
+
+Real time encryption and decryption webapp using the Advanced Encryption Standard (AES). AES is a U.S. Federal Information Processing Standard (FIPS). This standard was selected after a 5-year process where 15 competing designs were evaluated. Generates a 256-bit key (AES-256).
+
+======
+
+#### AngularJS, CryptoJS.
+
+
+![Screenshot](/images/screenshot_01.png "Cryptobal")
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,51 +2,39 @@
 
 angular.module('cryptoballApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma',
-      'LOL'
-    ];
-    $scope.message = "Message from main.js!";
+
     $scope.inputMessage = "";
-    $scope.extraMsg = "LOL";
-    $scope.newMsg = "";
 
     $scope.encrypted = "";
+    $scope.encryptedMsg = "";
     $scope.decrypted = "";
     $scope.secretPassphrase = "";
 
 //https://code.google.com/p/crypto-js/
 
-/*
-Example
-
-var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
-var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
-decrypted.toString(CryptoJS.enc.Utf8);
-
+/**
+ * encrypts string using pass phrase
+ * @return {Object} encrypted object
  */
-
-    /**
-     * [ concatenates two strings and assigns into a third parameter ]
-     * @return {string} [description]
-     */
-    $scope.concatString = function() {
-    	$scope.newMsg = CryptoJS.AES.encrypt($scope.inputMessage, $scope.extraMsg);
-    };
-
     $scope.encryptor = function() {
+      var encryptedObj = "";
       $scope.encrypted = CryptoJS.AES.encrypt($scope.inputMessage, $scope.secretPassphrase);
-      //var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+
       console.log('encrypted: ' + $scope.encrypted );
 
     };
 
+/**
+ * decrypts encrypted object, requires pass phrase
+ * @return {String}
+ */
     $scope.decryptor = function() {
-      $scope.decrypted = CryptoJS.AES.encrypt($scope.encryptedMessage, $scope.secretPassphrase);
-      //var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
-      console.log('decrypted: ' + $scope.decrypted );
+
+      var decrypted = CryptoJS.AES.decrypt($scope.encryptedMsg, $scope.secretPassphrase);
+      $scope.decrypted = decrypted.toString(CryptoJS.enc.Utf8);
+
+      console.log('decrypted: ' +  decrypted.toString(CryptoJS.enc.Utf8) );
+
     };
     
   });
